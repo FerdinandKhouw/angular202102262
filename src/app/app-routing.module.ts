6e8bb78618b0ae9e6240de1001cee520d7489b66
FerdinandKhouw <90260030+FerdinandKhouw@users.noreply.github.com>
2022-10-11 19:55:13 +0700

    Backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "admin", component: AdminComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent},
+  { path: "admin", component: AdminComponent},
+  { path: "header", component: HeaderComponent},
+  { path: "sidebar", component: SidebarComponent},
+  { path: "content", component: ContentComponent},
+  { path: "footer", component: FooterComponent},
+  { path: "", redirectTo: "login", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
